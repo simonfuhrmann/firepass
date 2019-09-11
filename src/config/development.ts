6ea463a36c808d7bdf8d28538ca01ab9fff9f023
Simon Fuhrmann <simonfuhrmann@gmx.de>
2019-09-10 22:27:27 -0700

    Added configuration files

---
+++ src/config/development.ts
@@ -0,0 +1,13 @@
+// The configuration values in this file are for development only.
+// They auto-fill form values so they do not have to be typed on page reload.
+
+export const devConfig = {
+  // Login email address and password.
+  // The values will be auto-filled in the login component.
+  loginEmail: '',
+  loginPassword: '',
+
+  // Unlock password for the database.
+  // If provided, the database is auto-unlocked after download.
+  unlockPassword: '',
+};
---
+++ src/config/firebase.ts
@@ -0,0 +1,14 @@
+// Your firebase configuration goes here.
+//
+// Here is how you get your config:
+// - Open the Firebase console: https://console.firebase.google.com/
+// - Click on the Firebase project for Firepass.
+// - Click the settings icon (Project settings) in the sidebar
+//
+// See here for more information:
+// https://support.google.com/firebase/answer/7015592
+export const firebaseConfig = {
+  apiKey: '',
+  databaseURL: '',
+  projectId: '',
+};
