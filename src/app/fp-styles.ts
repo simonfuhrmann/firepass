df7e591fae2e8e41b96ca0d0c79cfaa21ace35c2
Simon Fuhrmann <simonfuhrmann@gmx.de>
2019-09-18 22:23:45 -0700

    Add fire and ice colors to styles

--- src/app/fp-styles.ts
+++ src/app/fp-styles.ts
@@ -13,27 +13,30 @@ export const sharedStyles = css`
 
     --theme-color-dark: #1a1a1a;
     --theme-color-gray: #4e4e50;
-    --theme-color-accent1: #6f2232;
-    --theme-color-accent2: #950740;
-    --theme-color-accent3: #c3073f;
+    --theme-color-fire1: #6f2232;
+    --theme-color-fire2: #950740;
+    --theme-color-fire3: #c3073f;
+    --theme-color-ice1: #0b3f4f;
+    --theme-color-ice2: #14728f;
+    --theme-color-ice3: #1fafdb;
 
     --oxy-input-text-color: white;
     --oxy-input-background-color: rgba(0, 0, 0, 0.2);
     --oxy-input-background-color-focused: rgba(0, 0, 0, 0.3);
     --oxy-input-border-color: var(--separator-color);
-    --oxy-input-border-color-focused: var(--theme-color-accent3);
+    --oxy-input-border-color-focused: var(--theme-color-fire3);
     --oxy-input-box-shadow: 0 2px 7px black inset;
     --oxy-input-box-shadow-focused: var(--oxy-input-box-shadow),
-                                    0 0 5px var(--theme-color-accent2);
+                                    0 0 5px var(--theme-color-fire2);
 
     --oxy-textarea-text-color: white;
     --oxy-textarea-background-color: rgba(0, 0, 0, 0.2);
     --oxy-textarea-background-color-focused: rgba(0, 0, 0, 0.3);
     --oxy-textarea-border-color: var(--separator-color);
-    --oxy-textarea-border-color-focused: var(--theme-color-accent3);
+    --oxy-textarea-border-color-focused: var(--theme-color-fire3);
     --oxy-textarea-box-shadow: 0 2px 7px black inset;
     --oxy-textarea-box-shadow-focused: var(--oxy-input-box-shadow),
-                                    0 0 5px var(--theme-color-accent2);
+                                       0 0 5px var(--theme-color-fire2);
 
     --oxy-scrollbar-width: 12px;
     --oxy-scrollbar-track-color: #090909;
