0eb74b440171890e69d383a6c508886f1023b95e
Simon Fuhrmann <simonfuhrmann@gmx.de>
2025-02-11 20:03:21 -0800

    Set refetch timeout to 1 minute by default

--- src/config/application.ts
+++ src/config/application.ts
@@ -6,7 +6,7 @@ export const appConfig = {
   // Refetch the database from the server after some idle in the lock screen,
   // when the browser tab gets opened again. This prevents unlocking a stale
   // version of the encrypted database.
-  refetchTimeoutMs: 1000*20,//1000 * 60 * 15,  // 15 Minutes
+  refetchTimeoutMs: 1000 * 60 * 1,  // 1 Minute
 
   // The version of the application. This is mainly informal.
   appVersion: '1.0.0',
